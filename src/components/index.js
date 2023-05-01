6626b40aa71ba824b76e146c2570ede25908f93d
Damilare Agba <blankgodd33@gmail.com>
2023-05-01 11:43:10 +0100

    [update]: skills and experience component and refactoring

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as Experience } from './Experience';
+export { default as Skills } from './Skills';
 export * from './Icons';
 export * from './Utils';
