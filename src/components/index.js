b0daca747c242b04d98a101656ed0a76c5736cb6
Damilare Agba <blankgodd33@gmail.com>
2025-01-17 10:52:40 +0100

    [update]: brands ive worked with

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as Experience } from './Experience';
+export { default as Brands } from './Brands';
 export * from './Icons';
 export * from './Utils';
